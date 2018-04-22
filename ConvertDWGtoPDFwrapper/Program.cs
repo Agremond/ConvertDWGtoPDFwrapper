822b250c6cbeddc20b485377bf5ca8162bab8ee2
Agremond <mailboxgmn@gmail.com>
2018-04-22 23:33:43 +0300

    Добавьте файлы проекта.

---
+++ ConvertDWGtoPDFwrapper/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConvertDWGtoPDFwrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timemask = 0;
+            DateTime currDate = DateTime.Now;
+
+            DateTime oldDate = currDate.AddMinutes(timemask);
+
+            string path = @"c:\test";
+            if(!Directory.Exists(path))
+            {
+                Console.Write("Directory not exist.");
+                return;
+            }
+
+            try
+            {
+                bool convert = false;
+                List<string> files = Directory.EnumerateFiles(@"c:\test", "*.dwg*", SearchOption.AllDirectories).ToList<string>();
+                Console.WriteLine("Searching files...");
+                foreach (string currentFile in files)
+                {
+                    convert = false;
+                    try
+                    {
+                        
+                        FileInfo _file = new FileInfo(currentFile);
+                        
+                     
+                        if (_file.LastWriteTime < oldDate)
+                        {
+                            Console.WriteLine("File is found: " + _file.FullName);
+                            FileInfo _outfile = new FileInfo(Path.ChangeExtension(_file.FullName, ".pdf"));
+                            DirectoryInfo thisDir = new DirectoryInfo(_file.DirectoryName);
+                            thisDir = thisDir.CreateSubdirectory("pdf");
+
+                            if (_outfile.Exists)
+                            {
+                                if (_outfile.Length != 0)
+                                    continue;
+                                convert = true;
+                            }
+                            else
+                                convert = true;
+
+                            if(convert)
+                            {
+                                ;
+                            }
+
+                        }
+                    }
+                    catch (IOException)
+                    {
+                        Console.Write("Can not access to file" + currentFile);
+                    }
+
+                  
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+
+            Console.ReadKey();
+
+        }
+
+
+
+        
+    }
+}
---
+++ ConvertDWGtoPDFwrapper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ConvertDWGtoPDFwrapper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConvertDWGtoPDFwrapper")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("4419d426-0353-4705-875e-729d05ae88dc")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
